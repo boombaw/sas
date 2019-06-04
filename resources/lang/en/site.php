390f0752da32a3ef8d53cf999620c1ac9fbb9c04
boombaw <mrboombaw32@gmail.com>
2019-06-05 01:29:38 +0700

    SAS Application

--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -1,5 +1,41 @@
 <?php 
 
 return [
-	'main_menu_home' => 'Home'
+	'main_menu_home' 	=> 'Home',
+
+	/*General Setting*/
+	'company_title' 	=> 'Company Name',
+	'lang_setting' 		=> 'Language Setting',
+	'email_setting' 	=> 'Email Setting',
+	'logo' 				=> 'Logo',
+	'address1' 			=> 'Address 1',
+	'address2' 			=> 'Address 2',
+	'phone' 			=> 'Phone',
+	'fax' 				=> 'Fax',
+	'current_lang'		=> 'Current Language',
+	'smtp'				=> 'SMTP',
+	'smtp_port'			=> 'SMTP PORT',
+	'username'			=> 'Username',
+	'password'			=> 'Password',
+
+	/*Organization*/
+	'org_id' 			=> 'Organization Id',
+	'org_name'			=> 'Organization Name',
+	'org_parent'		=> 'Parent Id',
+	'org_mgmnt'			=> 'Organization Management',
+
+	/*Component*/
+	'act'				=> 'Action',
+	'add_data'			=> 'Add New Data',
+	'expand_panel'		=> 'Expand Panel',
+	'res_filter'		=> 'Reset Filter',
+
+
+
+	
+	// Button
+	'submit' 			=> 'Submit',
+	'add'				=> 'Add',
+
+	
 ];
